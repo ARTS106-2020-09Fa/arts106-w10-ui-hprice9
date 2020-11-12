41ac59da3ba20c41a0f8e214344d2bdc8e0ae15a
hprice9 <heyheatherprice@gmail.com>
2020-11-11 20:35:35 -0500

    pt4

---
+++ Assets/Inventory Display.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    typeIndex = Random.Range(0, 3);
+buttonText = GetComponentInChildren<Text>();
+buttonText.text = itemTypes[typeIndex];
+    {
+        
+    }
+}
---
+++ Assets/Inventory Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InventoryItem : MonoBehaviour
+    private Text buttonText;
+private string[] itemTypes = { "Armor", "Weapon", "Spell" };
+public int typeIndex;
+typeIndex = Random.Range(0, 3);
+buttonText = GetComponentInChildren <Text> ();
+buttonText.text = itemTypes[typeIndex];
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
